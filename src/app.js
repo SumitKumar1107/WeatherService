4e02932951f7627e2957a66b80afa0c410a3bfdc
EC2 Default User <ec2-user@ip-172-31-85-15.ec2.internal>
2019-08-11 18:14:30 +0000

    deploy

--- src/app.js
+++ src/app.js
@@ -1,89 +1,102 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
 const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const app=express()
-const port = process.env.PORT
+const app = express()
+const port = process.env.PORT || 3000
 
-// define paths for express config
-const publicdirectorypath = path.join(__dirname,'../public')
-const viewpath = path.join(__dirname,'../templates/views')
-const partialpath = path.join(__dirname,'../templates/partials')
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
-// setup handlebars engine and views location
-app.set('view engine','hbs')
-app.set('views',viewpath)
-hbs.registerPartials(partialpath)
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-// setup static directory to server
-app.use(express.static(publicdirectorypath))
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
-
-app.get('',(req,res)=>{
-    res.render('index',{
-        title:'Weather',
-        name:'Sumit Kumar'
-    })    
-}) 
-
-app.get('/about',(req,res) =>{
-    res.render('about',{
-        title:'About',
-        name:'Sumit kumar'
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Sumit Kumar'
     })
 })
 
-app.get('/help',(req,res) =>{
-    res.render('help',{
-        title:'Help',
-        name:'Sumit Kumar'
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Sumit Kumar'
     })
 })
 
-app.get('/help/*',(req,res) => {
-    res.render('404',{
-        title:'404',
-        name:'Sumit Kumar',
-        ErrorMessage:'Help Article not found'
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Sumit kumar'
     })
 })
 
-app.get('/weather',(req,res) =>{
-    if(!req.query.address){
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
         return res.send({
-            error:'you must provide an address'
+            error: 'You must provide an address!'
         })
     }
-    geocode(req.query.address, (error,{latitude, longitude, location} = {})=>{
-        if(error){
-            return res.send({error})
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
         }
-        forecast(latitude , longitude , (error,forecastdata) => {
-          if(error){
-            return res.send({error})   
-        }
-        res.send({
-            forecast: forecastdata,
-            location,
-            address: req.query.address
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
         })
-        
-    })
-})
-})
-
-app.get('*',(req,res) => {
-    res.render('404',{
-        title:'404',
-        name:'Sumit Kumar',
-        ErrorMessage:'page not found'
     })
 })
 
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
 
-app.listen(port, function(){
-   console.log("Server Has Started!!")
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Sumit Kumar',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Sumit Kumar',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
